db6fc512521a22f215b4649d3f16231d52a329aa
KyoKeityou <xu.jingchao@icloud.com>
2023-07-28 19:09:46 +0900

    update

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,7 +3,7 @@
   <div>
     <h1>雀活モック画面サイト</h1>
     <p>麻雀プロ用</p>
-    <!-- <router-link to="/proschedule">プロスケジュール</router-link> -->
+    <router-link to="./proschedule">プロスケジュール</router-link>
   </div>
 </template>
 
