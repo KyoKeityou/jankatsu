dcefbecc2189eb225a62befa77eb0d939fea7d40
KyoKeityou <xu.jingchao@icloud.com>
2023-07-30 21:03:07 +0900

    update

--- src/components/CreateSchedule.vue
+++ src/components/CreateSchedule.vue
@@ -1,75 +1,106 @@
 <template>
   <div class="create-schedule">
-    <!-- フォーム要素をここに記述します -->
-    <el-time-picker
-    v-model="startTime"
-    :picker-options="{
-        start: '00:00',
-        step: '00:30',
-        end: '23:30'
-    }"
-    ></el-time-picker>
-
-    <el-time-picker
-    v-model="endTime"
-    :picker-options="{
-        start: '00:30',
-        step: '00:30',
-        end: '23:59'
-    }"
-    ></el-time-picker>
-
-    <el-input v-model="title" placeholder="タイトル"></el-input>
-
-    <el-input v-model="location" placeholder="場所"></el-input>
-
-    <el-radio-group v-model="privateFlag">
-    <el-radio-button :label="true">プライベート</el-radio-button>
-    <el-radio-button :label="false">パブリック</el-radio-button>
-    </el-radio-group>
-
-    <el-button @click="submitForm">登録</el-button>
-    <el-button @click="cancelForm">キャンセル</el-button>
-  </div>  
+    <h2>Create Schedule</h2>
+    <el-form :model="form" :rules="rules" ref="form" label-width="100px">
+      <el-form-item label="日付(date)">
+        <el-date-picker v-model="form.date" type="date" placeholder="選択してください" @change="updateDay" />
+        <span>{{ selectedDay }}</span>
+      </el-form-item>
+      <el-form-item label="カテゴリ(category)">
+        <el-select v-model="form.category" placeholder="選択してください" @change="handleCategoryChange">
+          <el-option label="常勤" value="c1"></el-option>
+          <el-option label="ゲスト" value="c2"></el-option>
+          <el-option label="タイトル戦" value="c3"></el-option>
+          <el-option label="プライベート" value="c4"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="場所(location)">
+        <template v-if="!locationDisabled">
+          <el-select v-model="form.location" placeholder="選択してください">
+            <el-option label="ドラゴン雀荘渋谷店" value="l1"></el-option>
+            <el-option label="じゃんかつ池袋店" value="l2"></el-option>
+            <el-option label="麻雀BAR銀座" value="l3"></el-option>
+            <el-option label="麻雀BAR新橋" value="l4"></el-option>
+          </el-select>
+        </template>
+        <template v-else>
+          <el-input v-model="form.location" placeholder="フリー入力" />
+        </template>
+      </el-form-item>
+      <el-form-item label="メモ(memo)">
+        <el-input type="textarea" v-model="form.memo" />
+      </el-form-item>
+      <el-form-item label="開始時間(timefrom)">
+        <el-time-picker v-model="form.timefrom" :picker-options="timeOptions" />
+      </el-form-item>
+      <el-form-item label="終了時間(timeto)">
+        <el-time-picker v-model="form.timeto" :picker-options="timeOptions" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm">Submit</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
-  
+
 <script>
-import { ref } from 'vue';
-
 export default {
-  setup() {
-    const startTime = ref('');
-    const endTime = ref('');
-    const title = ref('');
-    const location = ref('');
-    const privateFlag = ref(false);
-
-    // フォーム送信処理
-    const submitForm = () => {
-    // フォーム送信のロジックをここに実装します
-    // 上記の ref 変数を使ってフォームデータにアクセスできます
-    };
-
-    // フォームキャンセル処理
-    const cancelForm = () => {
-    // フォームのキャンセルのロジックをここに実装します
-    // 例えば、前のページに戻るなどの処理を行います
-    };
-
+  data() {
     return {
-    startTime,
-    endTime,
-    title,
-    location,
-    privateFlag,
-    submitForm,
-    cancelForm,
+      form: {
+        date: null,
+        category: "",
+        location: "",
+        memo: "",
+        timefrom: null,
+        timeto: null,
+      },
+      selectedDay: "",
+      locationDisabled: false,
+      rules: {
+        date: [{ required: true, message: "日付を選択してください", trigger: "change" }],
+        category: [{ required: true, message: "カテゴリを選択してください", trigger: "change" }],
+        location: [{ required: true, message: "場所を選択または入力してください", trigger: "change" }],
+        memo: [{ required: true, message: "メモを入力してください", trigger: "blur" }],
+        timefrom: [{ required: true, message: "開始時間を選択してください", trigger: "change" }],
+        timeto: [{ required: true, message: "終了時間を選択してください", trigger: "change" }],
+      },
+      timeOptions: {
+        selectableRange: "18:00:00 - 24:00:00",
+      },
     };
+  },
+  methods: {
+    updateDay() {
+      if (this.form.date) {
+        const day = new Date(this.form.date).toLocaleDateString("ja-JP", { weekday: "short" });
+        this.selectedDay = day;
+      } else {
+        this.selectedDay = "";
+      }
+    },
+    handleCategoryChange(value) {
+      this.locationDisabled = value === "c1" || value === "c2";
+    },
+    submitForm() {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          // フォームのバリデーションが成功した場合、ここでフォームの送信処理を行います
+          console.log("Form submitted");
+        } else {
+          console.log("Form validation failed");
+          return false;
+        }
+      });
+    },
   },
 };
 </script>
-  
+
 <style>
-/* コンポーネント固有のスタイルをここに記述します（必要な場合のみ） */
+.create-schedule {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+}
 </style>
-  
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,7 +3,8 @@
   <div>
     <h1>雀活モック画面サイト</h1>
     <p>麻雀プロ用</p>
-    <router-link to="./proschedule">プロスケジュール</router-link>
+    <router-link to="./proschedule">月間スケジュール</router-link><br />
+    <router-link to="./createschedule">スケジュール登録（仕掛中）</router-link>
   </div>
 </template>
 
